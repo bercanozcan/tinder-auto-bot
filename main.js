2a8ef4d0beca19200b2c2d2ee4d74cb4925b4d0a
Bercan Özcan <bercanozcan@gmail.com>
2020-07-10 13:18:25 +0300

    Hello World!

---
+++ main.js
@@ -0,0 +1,3 @@
+SwipeRight = setInterval(function () {
+    document.querySelector("#content > div > div.App__body.H\\(100\\%\\).Pos\\(r\\).Z\\(0\\) > div > main > div.H\\(100\\%\\) > div > div > div.recsCardboard.W\\(100\\%\\).Mt\\(a\\).H\\(100\\%\\)--s.Px\\(10px\\)--s.Pos\\(r\\) > div > div.Pos\\(r\\).Py\\(16px\\).Px\\(4px\\).Px\\(8px\\)--ml.D\\(f\\).Jc\\(sb\\).Ai\\(c\\).Maw\\(375px\\)--m.Mx\\(a\\).Pe\\(n\\).Mt\\(-1px\\) > div:nth-child(4) > button").click()
+}, 1000)
